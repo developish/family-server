212c7395e77abcfdcf145fbac20566522859b78d
Brandon Wright <brandon@developish.com>
2018-01-01 16:05:44 -0700

    Save member names into database

--- server.py
+++ server.py
@@ -1,10 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+
+import psycopg2
+
+conn = psycopg2.connect("dbname=family_development user=%s" % os.environ['USER'])
+cur = conn.cursor()
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/members")
 def members():
-    members = ['Brandon', 'Becky', 'Violet', 'Zoey']
+    members = []
+
+    cur.execute("SELECT id, name FROM members")
+    for record in cur:
+        members.append(record[1])
+
     return jsonify(members)
+
+@app.route("/members", methods=['POST'])
+def create_member():
+    name = request.json['name']
+
+    cur.execute("INSERT INTO members (name) VALUES (%s);", (name,))
+    conn.commit()
+    return "OK"
